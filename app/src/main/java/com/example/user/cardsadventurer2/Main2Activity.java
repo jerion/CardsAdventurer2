b56648e7eab2d7c8c9fd160d1aed8355b010f257
jerion <harry26934719@gmail.com>
2018-12-26 07:28:08 +0800

    main1→main2

--- app/src/main/java/com/example/user/cardsadventurer2/Main2Activity.java
+++ app/src/main/java/com/example/user/cardsadventurer2/Main2Activity.java
@@ -2,12 +2,30 @@ package com.example.user.cardsadventurer2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import charactor.Charactor;
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView level;
+    TextView hp, hpn;
+    TextView mp, mpn;
+    TextView exp, expn;
+    TextView money;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        level = findViewById(R.id.level_player);
+        hpn = findViewById(R.id.hp_player);
     }
+
+    Charactor monster[] = { new Charactor("蝙蝠", 1,3,1,1,3),
+            new Charactor("樹精", 2, 12, 3, 3, 7),
+            new Charactor("梅杜莎",3, 15, 4, 6, 20)};
+
+    Charactor boss[] = {new Charactor("劍客", 4, 48, 4, 0, 0)};
 }
--- app/src/main/java/com/example/user/cardsadventurer2/MainActivity.java
+++ app/src/main/java/com/example/user/cardsadventurer2/MainActivity.java
@@ -1,31 +1,51 @@
 package com.example.user.cardsadventurer2;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.Serializable;
 
 import charactor.Charactor;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    Button bstart;
+    Button bcontinue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        bstart = findViewById(R.id.btn_sta);
+        bcontinue = findViewById(R.id.btn_con);
+
+        final Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+
+        Bundle bundle = new Bundle();
+        Bundle bundle1 = new Bundle();
+
+        bundle.putSerializable("new", (Serializable) player);
+
+        bstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
+        bcontinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
-
-    Charactor witch;
-    Charactor monster1;
-
-    int level = witch.level;
-
-    public void setWitch(Charactor witch) {
-        this.witch = witch;
-        witch.setcharactor(1,20,1,2);
-    }
-
-    public void setMonster1(Charactor monster1) {
-        this.monster1 = monster1;
-        monster1.setcharactor(1,5,1,1);
-    }
+    Charactor player = new Charactor("愚蠢的人", 1, 20, 1, 2,25);
 }
 
