2997cf5c2b191dd6666562fee6bf67e81eca1099
jerion <harry26934719@gmail.com>
2018-12-25 17:58:54 +0800

    charactor.jar
    set charactor

--- app/src/main/java/com/example/user/cardsadventurer2/MainActivity.java
+++ app/src/main/java/com/example/user/cardsadventurer2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.user.cardsadventurer2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import charactor.Charactor;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +12,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    Charactor witch;
+    Charactor monster1;
+
+    int level = witch.level;
+
+    public void setWitch(Charactor witch) {
+        this.witch = witch;
+        witch.setcharactor(1,20,1,2);
+    }
+
+    public void setMonster1(Charactor monster1) {
+        this.monster1 = monster1;
+        monster1.setcharactor(1,5,1,1);
+    }
 }
+
