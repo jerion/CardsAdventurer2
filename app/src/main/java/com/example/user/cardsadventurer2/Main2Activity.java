6d169c51383ac4672b792d32e4148090e48671ff
105360093 <iamsam980316@gmail.com>
2018-12-26 22:01:27 +0800

    About to give up

--- app/src/main/java/com/example/user/cardsadventurer2/Main2Activity.java
+++ app/src/main/java/com/example/user/cardsadventurer2/Main2Activity.java
@@ -2,8 +2,29 @@ package com.example.user.cardsadventurer2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView target;
+    TextView conversation;
+    TextView name_item;
+    TextView intro_item;
+    TextView level;
+    TextView hp, hpn;
+    TextView mp, mpn;
+    TextView exp, expn;
+    TextView money;
+    Button card;
+    Button skill;
+    TextView card_left;
+    ImageButton event1, event2, event3;
+    TextView ntro_event;
+    ImageView item1, item2, item3;
+    Button get1, get2, get3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/user/cardsadventurer2/MainActivity.java
+++ app/src/main/java/com/example/user/cardsadventurer2/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.user.cardsadventurer2;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.Serializable;
+
+import charactor.Charactor;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bstart;
+    Button bcontinue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        bstart = findViewById(R.id.btn_sta);
+        bcontinue = findViewById(R.id.btn_con);
+
+        int[] player_stats = {player.level, player.health,player.mana,player.experience,player.money};
+        int[] old_player;
+
+        final Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+
+        Bundle bundle = new Bundle();
+        bundle.putIntArray("new",player_stats);
+
+        bstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
+        bcontinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+    Charactor player = new Charactor("愚蠢的人", 1, 20, 1, 2,25);
 }
