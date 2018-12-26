5e0a3ced15d3fc35d5e998d2e49e41fec5542d3f
105360093 <iamsam980316@gmail.com>
2018-12-26 22:59:05 +0800

    findViewById

--- app/src/main/java/com/example/user/cardsadventurer2/Main2Activity.java
+++ app/src/main/java/com/example/user/cardsadventurer2/Main2Activity.java
@@ -22,7 +22,7 @@ public class Main2Activity extends AppCompatActivity {
     Button skill;
     TextView card_left;
     ImageButton event1, event2, event3;
-    TextView ntro_event;
+    TextView intro_event;
     ImageView item1, item2, item3;
     Button get1, get2, get3;
 
@@ -30,5 +30,31 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        target = findViewById(R.id.name_target);
+        conversation = findViewById(R.id.conversation);
+        name_item = findViewById(R.id.name_item);
+        intro_item = findViewById(R.id.intro_item);
+        level = findViewById(R.id.level_player);
+        hp = findViewById(R.id.hp_player);
+        hpn = findViewById(R.id.hp_player);
+        mp = findViewById(R.id.sp_player);
+        mpn = findViewById(R.id.sp_player);
+        exp = findViewById(R.id.exp_player);
+        expn = findViewById(R.id.exp_player);
+        money = findViewById(R.id.gold);
+        card = findViewById(R.id.btn_card_player);
+        skill = findViewById(R.id.btn_skill);
+        card_left = findViewById(R.id.card_left);
+        event1 = findViewById(R.id.imbtn_event1);
+        event2 = findViewById(R.id.imbtn_event2);
+        event3 = findViewById(R.id.imbtn_event3);
+        intro_event = findViewById(R.id.intro_event);
+        item1 = findViewById(R.id.im_item1);
+        item2 = findViewById(R.id.im_item2);
+        item3 = findViewById(R.id.im_item3);
+        get1 = findViewById(R.id.btn_get1);
+        get2 = findViewById(R.id.btn_get2);
+        get3 = findViewById(R.id.btn_get3);
     }
 }
