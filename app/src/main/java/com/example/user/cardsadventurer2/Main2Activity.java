93e3ed89ced709509c1d59e98c5cf0ebc2e41979
105360093 <iamsam980316@gmail.com>
2018-12-27 00:57:44 +0800

    Update Main2Activity.java

--- app/src/main/java/com/example/user/cardsadventurer2/Main2Activity.java
+++ app/src/main/java/com/example/user/cardsadventurer2/Main2Activity.java
@@ -9,8 +9,11 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import card.Atk_Card;
 import card.Card;
+import card.Effect_Card;
 import charactor.Charactor;
+import stage.Stage;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -48,13 +51,11 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-<<<<<<< HEAD
         level = findViewById(R.id.level_player);
         hpn = findViewById(R.id.hp_player);
 
         bundle3.putIntArray("monster", monster_stats[0]);
 
-=======
         target = findViewById(R.id.name_target);
         conversation = findViewById(R.id.conversation);
         name_item = findViewById(R.id.name_item);
@@ -80,17 +81,49 @@ public class Main2Activity extends AppCompatActivity {
         get1 = findViewById(R.id.btn_get1);
         get2 = findViewById(R.id.btn_get2);
         get3 = findViewById(R.id.btn_get3);
->>>>>>> upstream/master
     }
     Charactor monster[] = {
-            new Charactor("蝙蝠", 1,3,1,1,3),
+            new Charactor("蝙蝠", 1, 3,1,1,3),
             new Charactor("樹精", 2, 12, 3, 3, 7),
-            new Charactor("梅杜莎",3, 15, 4, 6, 20)};
+            new Charactor("梅杜莎", 3, 15, 4, 6, 20)};
 
     Charactor boss[] = {new Charactor("劍客", 4, 48, 4, 0, 0)};
 
-    Card cardlist[] = {
-            new Card("火球", "火", 1, 1, 3),
-            new Card("火球", "火", 2, 2, 8)
+    Atk_Card normal_atk[] = {
+            new Atk_Card("攻擊", "無", 1, 0, 2),
+            new Atk_Card("攻擊", "無", 2, 0, 4),
+            new Atk_Card("攻擊", "無", 3, 0, 6),
+            new Atk_Card("攻擊", "無", 4, 0, 8),
+    };
+
+    Atk_Card fire_ball[] = {
+            new Atk_Card("火球", "火", 1, 1, 3),
+            new Atk_Card("火球", "火", 2, 2, 6),
+            new Atk_Card("火球", "火", 3, 3, 9),
+            new Atk_Card("火球", "火", 4, 4, 12),
+    };
+
+    Atk_Card ice_ball[] = {
+            new Atk_Card("冰彈", "冰", 1, 1, 3),
+            new Atk_Card("冰彈", "冰", 2, 2, 6),
+            new Atk_Card("冰彈", "冰", 3, 3, 9),
+            new Atk_Card("冰彈", "冰", 4, 4, 12),
+    };
+
+    Effect_Card meditation[] = {
+            new Effect_Card("冥想", "無", 1, 1, 3, 0),
+            new Effect_Card("冥想", "無", 2, 2, 6, 0),
+    };
+
+    Effect_Card corpus[] = {
+            new Effect_Card("法典", "無", 1, 1,3, 0),
+            new Effect_Card("法典", "無", 2, 2,6, 0),
+    };
+
+    Stage stage[] = {
+            new Stage("蝙蝠","蝙蝠又名蟙䘃，是對翼手目動物的通稱，翼手目是哺乳動物中僅次於齧齒目動物的第二大類群，現生種共有19科185屬962種，除極地和大洋中的一些島嶼外，分布遍於全世界。"),
+            new Stage("樹精","樹精是一種神話裡面描述存在的生物。在中國神話中，樹木也由於生存時間很長，所以得以采天地靈氣，受日月之精華，從而變化成妖。"),
+            new Stage("梅杜莎","美杜莎是戈耳貢女妖之一，外觀描述是纏著龍鱗的頭，像野豬一般的獠牙，青銅的手爪，金色的翅膀。任何直望美杜莎雙眼的人都會變成石像。"),
+            new Stage("劍客","劍客為行俠仗義的人。"),
     };
 }
